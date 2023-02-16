9630e287a06f7a4527ca4cc614e665bb5d39aa01
Vinicius Da Silva Meireles <viniciusmeirelesbr@gmail.com>
2023-02-16 02:57:51 -0300

    connect and print

--- main.py
+++ main.py
@@ -1,37 +1,80 @@
-from datetime import datetime
-import os
-import time
-from flask import Flask
-from flask import request
-from subprocess import check_output
+from escpos.printer import Network
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
+host = '192.168.1.100'  # Printer IP Address
 
-@app.route("/imprimir")
-def printer_connect():
-    print(request.args)
+try:
+    printer = Network(host=host)
+except Exception as excp:
+    print(excp)
+
+
+def reconnect():
+    global printer
     try:
-        check_output("cmd /c type c:\\printer-temp\\ticket-printer.txt > \\\\localhost\\ticket-printer", shell=True)
-    except:
-        print('impressora não encontrada')
-    return "<p>Hello, World!</p>"
+        if not printer:
+            printer = Network(host=host)
+        if not printer.device:
+            printer.open()
+        return True
+    except Exception as excp:
+        print(excp)
+        return False
+
+
+@app.route('/imprimir', methods=['GET', 'POST'])
+def printer_connect():
+    # if request.method == 'POST' and request.is_json:
+    if request.method == 'POST':
+        global printer
+
+        if (not printer or not printer.device) and not reconnect():
+            return jsonify(status=404, data={'error': 'Unable to connect to printer'})
+
+        header = request.args.get('header')
+        footer = request.args.get('footer')
+        code = request.args.get('code')
+        client_name = request.args.get('client_name')
+        qr_code = request.args.get('qr_code')
+        created_date = request.args.get('created_date')
+        services = request.args.get('services')
+
+        # print(header)
+        # print(footer)
+        # print(code)
+        # print(client_name)
+        # print(qr_code)
+        # print(created_date)
+        # print(services)
+
+        # Print text
+        if header:
+            printer.set(font="a", height=2, align="center")
+            printer.text(header + '\n')
+            printer.set(font="a", height=2, align="left")
+        if client_name:
+            printer.text(client_name + '\n')
+        if code:
+            printer.text(code + '\n')
+        if created_date:
+            printer.text(created_date + '\n')
+        if services:
+            printer.text(services + '\n')
+
+        # Print QR Code
+        if qr_code:
+            printer.qr(content=qr_code)
+
+        if footer:
+            printer.text(footer + '\n')
+
+        #  Cut paper
+        printer.cut()
+
+        return jsonify(status=200)
+    return jsonify(status=404)
+
 
 if __name__ == '__main__':
-    datetime_obj = datetime.now()
-    print(f"datetime_obj = {datetime_obj}")
-    print(f"datetime_obj type = {type(datetime_obj)}")
-
-    folder_name = datetime_obj.strftime("%Y-%m-%d %H:%M:%S.%f")
-    # ':' is not allowed in folder naming in Windows, so change it to '_'
-    folder_name = folder_name.replace(':', '_')
-    print(f"Folder name will be: {folder_name}")
-    print(f"folder_name type = {type(folder_name)}")
-
-    cwd = os.getcwd()
-    print(f"Current working directory: {cwd}")
-
-    folder_directory = cwd + "\\" + folder_name
-    print(f"Will try to create new folder : {folder_directory}")
-    time.sleep(5)
-    print("Finish!")
     app.run()
